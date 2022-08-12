832a6aa088080c525a7cfe8cdd581211767834b1
Barnabe <barnabedikartola@gmail.com>
2022-08-12 12:38:18 -0300

    escrevi comentarios

--- xanmod.sh
+++ xanmod.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+##### Não Editar Start #####
 xanmod=
 webhooks() {
 echo '
@@ -11,15 +13,21 @@ curl -X POST \
 bash -x run-webhooks-aur.sh
 rm run-webhooks-aur.sh
 }
+##### NÃO Editar End #####
+
+
 
 ## STABLE ##
+#versão online no site da AUR
 major=$(curl -s https://aur.archlinux.org/cgit/aur.git/tree/PKGBUILD?h=linux-xanmod | sed 's/<[^>]*>//g' | grep _major= | cut -d "=" -f2 | sed 's|\.||g' | sed 's|-||g')
 pkgver=$(curl -s https://aur.archlinux.org/cgit/aur.git/tree/PKGBUILD?h=linux-xanmod | sed 's/<[^>]*>//g' | grep pkgver= | cut -d "=" -f2 | sed 's|\.||g' | sed 's|-||g' | cut -d "}" -f2)
 pkgrel=$(curl -s https://aur.archlinux.org/cgit/aur.git/tree/PKGBUILD?h=linux-xanmod | sed 's/<[^>]*>//g' | grep xanmod= | cut -d "=" -f2 | sed 's|\.||g' | sed 's|-||g')
 versite=$major$pkgver$pkgrel
 
+#versão do repositorio do biglinux
 verrepo=$(pacman -Ss xanmod | grep biglinux-stable | grep -v headers | egrep -v "lts|rt|tt|edge" | cut -d " " -f2  | sed 's/\.//g' | sed 's/\-//')
 
+#se versão do site foi maior que a versão do repo local
 if [ "$versite" -gt "$verrepo" ]; then
     echo "Envia xanmod stable"
     xanmod=linux-xanmod
@@ -27,13 +35,16 @@ if [ "$versite" -gt "$verrepo" ]; then
 fi
 
 ## EDGE ##
+#versão online no site da AUR
 major=$(curl -s https://aur.archlinux.org/cgit/aur.git/tree/PKGBUILD?h=linux-xanmod-edge | sed 's/<[^>]*>//g' | grep _major= | cut -d "=" -f2 | sed 's|\.||g' | sed 's|-||g')
 pkgver=$(curl -s https://aur.archlinux.org/cgit/aur.git/tree/PKGBUILD?h=linux-xanmod-edge | sed 's/<[^>]*>//g' | grep pkgver= | cut -d "=" -f2 | sed 's|\.||g' | sed 's|-||g' | cut -d "}" -f2)
 pkgrel=$(curl -s https://aur.archlinux.org/cgit/aur.git/tree/PKGBUILD?h=linux-xanmod-edge | sed 's/<[^>]*>//g' | grep xanmod= | cut -d "=" -f2 | sed 's|\.||g' | sed 's|-||g')
 edgeversite=$major$pkgver$pkgrel
 
+#versão do repositorio do biglinux
 edgeverrepo=$(pacman -Ss xanmod | grep biglinux-stable | grep -v headers | egrep -v "lts|rt|tt" | grep edge | cut -d " " -f2  | sed 's/\.//g' | sed 's/\-//')
 
+#se versão do site foi maior que a versão do repo local
 if [ "$edgeversite" -gt "$edgeverrepo" ]; then
     echo "Envia edge"
     xanmod=linux-xanmod-edge
@@ -41,13 +52,16 @@ if [ "$edgeversite" -gt "$edgeverrepo" ]; then
 fi
 
 ## LTS ##
+#versão online no site da AUR
 major=$(curl -s https://aur.archlinux.org/cgit/aur.git/tree/PKGBUILD?h=linux-xanmod-lts | sed 's/<[^>]*>//g' | grep _major= | cut -d "=" -f2 | sed 's|\.||g' | sed 's|-||g')
 pkgver=$(curl -s https://aur.archlinux.org/cgit/aur.git/tree/PKGBUILD?h=linux-xanmod-lts | sed 's/<[^>]*>//g' | grep pkgver= | cut -d "=" -f2 | sed 's|\.||g' | sed 's|-||g' | cut -d "}" -f2)
 pkgrel=$(curl -s https://aur.archlinux.org/cgit/aur.git/tree/PKGBUILD?h=linux-xanmod-lts | sed 's/<[^>]*>//g' | grep xanmod= | cut -d "=" -f2 | sed 's|\.||g' | sed 's|-||g')
 ltsversite=$major$pkgver$pkgrel
 
+#versão do repositorio do biglinux
 ltsverrepo=$(pacman -Ss xanmod | grep biglinux-stable | grep -v headers | egrep -v "edge|rt|tt" | grep lts | cut -d " " -f2  | sed 's/\.//g' | sed 's/\-//')
 
+#se versão do site foi maior que a versão do repo local
 if [ "$ltsversite" -gt "$ltsverrepo" ]; then
     echo "Envia lts"
     xanmod=linux-xanmod-lts
@@ -55,13 +69,16 @@ if [ "$ltsversite" -gt "$ltsverrepo" ]; then
 fi
 
 ## RT ##
+#versão online no site da AUR
 major=$(curl -s https://aur.archlinux.org/cgit/aur.git/tree/PKGBUILD?h=linux-xanmod-rt | sed 's/<[^>]*>//g' | grep _major= | cut -d "=" -f2 | sed 's|\.||g' | sed 's|-||g')
 pkgver=$(curl -s https://aur.archlinux.org/cgit/aur.git/tree/PKGBUILD?h=linux-xanmod-rt | sed 's/<[^>]*>//g' | grep pkgver= | cut -d "=" -f2 | sed 's|\.||g' | sed 's|-||g' | cut -d "}" -f2)
 pkgrel=$(curl -s https://aur.archlinux.org/cgit/aur.git/tree/PKGBUILD?h=linux-xanmod-rt | sed 's/<[^>]*>//g' | grep xanmod= | cut -d "=" -f2 | sed 's|\.||g' | sed 's|-||g')
 rtversite=$major$pkgver$pkgrel
 
+#versão do repositorio do biglinux
 rtverrepo=$(pacman -Ss xanmod | grep biglinux-stable | grep -v headers | egrep -v "lts|edge|tt" | grep rt | cut -d " " -f2  | sed 's/\.//g' | sed 's/\-//')
 
+#se versão do site foi maior que a versão do repo local
 if [ "$rtversite" -gt "$rtverrepo" ]; then
     echo "Envia rt"
     xanmod=linux-xanmod-rt
@@ -69,13 +86,16 @@ if [ "$rtversite" -gt "$rtverrepo" ]; then
 fi
 
 ## RT ##
+#versão online no site da AUR
 major=$(curl -s https://aur.archlinux.org/cgit/aur.git/tree/PKGBUILD?h=linux-xanmod-tt | sed 's/<[^>]*>//g' | grep _major= | cut -d "=" -f2 | sed 's|\.||g' | sed 's|-||g')
 pkgver=$(curl -s https://aur.archlinux.org/cgit/aur.git/tree/PKGBUILD?h=linux-xanmod-tt | sed 's/<[^>]*>//g' | grep pkgver= | cut -d "=" -f2 | sed 's|\.||g' | sed 's|-||g' | cut -d "}" -f2)
 pkgrel=$(curl -s https://aur.archlinux.org/cgit/aur.git/tree/PKGBUILD?h=linux-xanmod-tt | sed 's/<[^>]*>//g' | grep xanmod= | cut -d "=" -f2 | sed 's|\.||g' | sed 's|-||g')
 ttversite=$major$pkgver$pkgrel
 
+#versão do repositorio do biglinux
 ttverrepo=$(pacman -Ss xanmod | grep biglinux-stable | grep -v headers | egrep -v "lts|edge|rt" | grep tt | cut -d " " -f2  | sed 's/\.//g' | sed 's/\-//')
 
+#se versão do site foi maior que a versão do repo local
 if [ "$ttversite" -gt "$ttverrepo" ]; then
     echo "Envia tt"
     xanmod=linux-xanmod-tt
