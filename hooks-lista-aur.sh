32e373bb18a39c3834eac58e18a31a255c9c8f55
Barnabe <barnabedikartola@gmail.com>
2022-08-17 07:11:25 -0300

    manda pro testing

--- hooks-lista-aur.sh
+++ hooks-lista-aur.sh
@@ -7,7 +7,7 @@ echo '
 curl -X POST \
 -H "Accept: application/json" \
 -H "Authorization: token '$CHAVE'" \
---data '"'{"'"event_type"'": "'"'AUR/$AUR'"'", "'"client_payload"'": { "'"pkgbuild"'": "'""'", "'"branch"'": "'"'stable'"'", "'"url"'": "'"https://aur.archlinux.org/'$AUR'"'", "'"version"'": "'"1.2.3"'"}}'"' \
+--data '"'{"'"event_type"'": "'"'AUR/$AUR'"'", "'"client_payload"'": { "'"pkgbuild"'": "'""'", "'"branch"'": "'"'testing'"'", "'"url"'": "'"https://aur.archlinux.org/'$AUR'"'", "'"version"'": "'"1.2.3"'"}}'"' \
 'https://api.github.com/repos/BigLinux-Package-Build/build-package/dispatches'' > run-webhooks-aur.sh
 
 bash -x run-webhooks-aur.sh
@@ -37,7 +37,7 @@ for i in $(cat lista-auto-hooks); do pkgname=$i
         echo "AUR ""$pkgname"="$versite"
         echo "Repo ""$pkgname"="$verrepo"
         AUR=$pkgname
-        #webhooks
+        webhooks
     else
     echo "Versão do $pkgname é igual !"
     fi
