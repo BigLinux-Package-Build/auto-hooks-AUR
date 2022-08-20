504a5adef5f2cb3c1db7b8792a4cf98094b4e9b3
Barnabe <barnabedikartola@gmail.com>
2022-08-20 09:55:17 -0300

    if vazio

--- hooks-lista-aur.sh
+++ hooks-lista-aur.sh
@@ -21,6 +21,9 @@ rm run-webhooks-aur.sh
 
 
 for i in $(cat lista-auto-hooks); do pkgname=$i
+    if [ "$i" = "" ];then
+        exit
+    fi
     #versão online no site da AUR
     pkgver=$(curl -s https://aur.archlinux.org/cgit/aur.git/tree/PKGBUILD?h=$pkgname | sed 's/<[^>]*>//g' | grep pkgver= | cut -d "=" -f2 | sed 's|\.||g' | sed 's|\_||g'| sed 's|-||g' | cut -d "}" -f2  | sed 's/&quot;//g' | sed 's|_||g')
     pkgrel=$(curl -s https://aur.archlinux.org/cgit/aur.git/tree/PKGBUILD?h=$pkgname | sed 's/<[^>]*>//g' | grep pkgrel= | cut -d "=" -f2 | sed 's|\.||g' | sed 's|-||g')
