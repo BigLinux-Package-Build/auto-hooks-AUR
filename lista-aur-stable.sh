3a0acc17dbcdce751f33ea7b9b1a6fb16b456a81
Barnabe <barnabedikartola@gmail.com>
2022-10-08 20:51:31 -0300

    nova forma de pegar versão do aur v4

--- lista-aur-stable.sh
+++ lista-aur-stable.sh
@@ -21,37 +21,36 @@ rm run-webhooks-aur.sh
 
 
 for i in $(cat lista-auto-hooks-stable); do pkgname=$i
-    if [ "$i" = "" -o -n "$(echo $i | grep \#)" ];then
-        exit
-    fi
-    #versão do AUR
-    veraur=
-    git clone https://aur.archlinux.org/${i}.git
-    cd $i 
-    source PKGBUILD
-    veraur=$pkgver-$pkgrel
-    cd ..
-    
-    #versão do repositorio do biglinux
-    if [ "$REPO" = "testing" ]; then
-        repo=biglinux-testing
-    elif [ "$REPO" = "stable" ]; then
-        repo=biglinux-stable
-    fi
-    verrepo=$(pacman -Ss $pkgname | grep $repo | grep -v "$pkgname-" | grep -v "\-$pkgname" | grep "$pkgname" | cut -d " " -f2 | cut -d ":" -f2)
-    if [ "$veraur" != "$verrepo" ]; then
-        verrepo=$(pacman -Ss $pkgname | grep biglinux-stable | grep -v "$pkgname-" | grep -v "\-$pkgname" | grep "$pkgname" | cut -d " " -f2 | cut -d ":" -f2)
-    fi
-    
-    #se versão do AUR foi maior que a versão do repo local
-    if [ "$veraur" != "$verrepo" ]; then
-        echo -e "Enviando \033[01;31m$pkgname\033[0m para Package Build"
-        echo " AUR ""$pkgname"="$veraur"
-        echo "Repo ""$pkgname"="$verrepo"
-        AUR=$pkgname
-#         webhooks
-    else
-        echo "Versão do $pkgname é igual !"
+    if [ "$i" != "" -o -z "$(echo $i | grep \#)" ];then
+        #versão do AUR
+        veraur=
+        git clone https://aur.archlinux.org/${i}.git
+        cd $i 
+        source PKGBUILD
+        veraur=$pkgver-$pkgrel
+        cd ..
+        
+        #versão do repositorio do biglinux
+        if [ "$REPO" = "testing" ]; then
+            repo=biglinux-testing
+        elif [ "$REPO" = "stable" ]; then
+            repo=biglinux-stable
+        fi
+        verrepo=$(pacman -Ss $pkgname | grep $repo | grep -v "$pkgname-" | grep -v "\-$pkgname" | grep "$pkgname" | cut -d " " -f2 | cut -d ":" -f2)
+        if [ "$veraur" != "$verrepo" ]; then
+            verrepo=$(pacman -Ss $pkgname | grep biglinux-stable | grep -v "$pkgname-" | grep -v "\-$pkgname" | grep "$pkgname" | cut -d " " -f2 | cut -d ":" -f2)
+        fi
+        
+        #se versão do AUR foi maior que a versão do repo local
+        if [ "$veraur" != "$verrepo" ]; then
+            echo -e "Enviando \033[01;31m$pkgname\033[0m para Package Build"
+            echo " AUR ""$pkgname"="$veraur"
+            echo "Repo ""$pkgname"="$verrepo"
+            AUR=$pkgname
+            #webhooks
+        else
+            echo "Versão do $pkgname é igual !"
+        fi
     fi
 done
 
