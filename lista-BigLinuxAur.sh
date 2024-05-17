4c81ba7c72d8502226936a15cde311bacb147d7d
gitbarnabedikartola <110747604+gitbarnabedikartola@users.noreply.github.com>
2024-05-17 16:07:27 -0300

    add new packages, remove geticons

--- lista-BigLinuxAur.sh
+++ lista-BigLinuxAur.sh
@@ -13,6 +13,10 @@ sleep 10
 webhooks
 }
 
+newRepo(){
+curl -sH "Authorization: token $CHAVE" -H "Accept: application/vnd.github.baptiste-preview+json" --data '{"owner":"BigLinuxAur","name":"'$pkgname'"}' https://api.github.com/repos/BigLinuxAur/aurTemplate/generate > /dev/null
+}
+
 repo=testing
 sed -i 's/#.*$//' BigLinuxAur-${repo}
 sed -i '/^$/d' BigLinuxAur-${repo}
@@ -21,6 +25,18 @@ for pkgname in $(cat BigLinuxAur-${repo}); do
   #versão do repositorio BigLinux
   verrepo=
   verrepo=$(pacman -Ss $pkgname | grep biglinux-${repo} | grep -v "$pkgname-" | grep -v "\-$pkgname" | grep "$pkgname" | cut -d "/" -f2 | grep -w $pkgname | cut -d " " -f2 | cut -d ":" -f2 | sed 's/\.//g;s/-//g')
+
+  # Verificar se repo existe no BigLinuxAur
+  if [ "$(curl -s -o /dev/null -w "%{http_code}" https://api.github.com/repos/BigLinuxAur/$pkgname)" != "200" ];then
+    echo -e "Criando repo \033[01;31m$pkgname\033[0m no GitHub"
+    newRepo
+    # Esperar fazer o pull do AUR
+    while [ "$(curl -s -o /dev/null -w "%{http_code}" https://api.github.com/repos/BigLinuxAur/$pkgname/contents/$pkgname)" != "200" ]; do
+      sleep 3
+    done
+    # Passar para o proximo da lista
+    continue
+  fi
 
   sleep 1
 
@@ -52,27 +68,23 @@ for pkgname in $(cat BigLinuxAur-${repo}); do
   # MSG de ERRO
   if [ -z "$veraur" ];then
     echo -e '\033[01;31m!!!ERRRRRO!!!\033[0m' $pkgname '\033[01;31m!!!ERRRRRO!!!\033[0m'
-  fi
-
   # Enviar caso não encontre no repo
-  if [ -z "$verrepo" ];then
+  elif [ -z "$verrepo" ];then
     sendWebHooks
-  else
-    # se contiver apenas numeros ou se for com hash
-    if [[ $veraur =~ ^[0-9]+$ ]]; then
-      if [ "$veraur" -gt "$verrepo" ]; then
-        sendWebHooks
-      else
-        echo "Versão do $pkgname é igual !"
-        sleep 1
-      fi
+  # se contiver apenas numeros ou se for com hash
+  elif [[ $veraur =~ ^[0-9]+$ ]]; then
+    if [ "$veraur" -gt "$verrepo" ]; then
+      sendWebHooks
     else
-      if [ "$veraur" != "$verrepo" ]; then
-        sendWebHooks
-      else
-        echo "Versão do $pkgname é igual !"
-        sleep 1
-      fi
+      echo "Versão do $pkgname é igual !"
+      sleep 1
+    fi
+  else
+    if [ "$veraur" != "$verrepo" ]; then
+      sendWebHooks
+    else
+      echo "Versão do $pkgname é igual !"
+      sleep 1
     fi
   fi
 echo
