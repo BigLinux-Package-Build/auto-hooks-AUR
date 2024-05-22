242f3d79a287ec033eeb23ff320007e71ea3ab78
gitbarnabedikartola <110747604+gitbarnabedikartola@users.noreply.github.com>
2024-05-22 18:37:05 -0300

    echo pkgname

--- BigLinuxAur.sh
+++ BigLinuxAur.sh
@@ -115,6 +115,8 @@ for p in $(gh repo list BigLinuxAur --limit 1000 | awk '{print $1}' | cut -d "/"
   cd ..
   rm -r $pkgname
 
+  echo "pkgname=$pkgname"
+
   # MSG de ERRO
   if [ -z "$veraur" ];then
     echo -e '\033[01;31m!!!ERRRRRO!!!\033[0m' $pkgname não encontrado '\033[01;31m!!!ERRRRRO!!!\033[0m'
