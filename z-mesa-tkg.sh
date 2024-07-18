c9948a8a18ce7b9b34e4a900ee03b3427abeba58
gitbarnabedikartola <gitbarnabedikartola@gmail.com>
2024-07-18 12:58:17 -0300

    transfer mesa-tkg-git to biglinuxAur repo

--- z-mesa-tkg.sh
+++ z-mesa-tkg.sh
@@ -3,62 +3,26 @@
 ##### Não Editar Start #####
 #mesa-git
 AUR=
+# webhooks() {
+# #mesa-tkg-git
+# echo '
+# curl -X POST \
+# -H "Accept: application/json" \
+# -H "Authorization: token '$CHAVE'" \
+# --data '"'{"'"event_type"'": "'"'TKG/$AUR'"'", "'"client_payload"'": { "'"pkgbuild"'": "'""'", "'"branch"'": "'"'stable'"'", "'"url"'": "'"https://github.com/Frogging-Family/mesa-git"'", "'"version"'": "'"1.2.3"'"}}'"' \
+# 'https://api.github.com/repos/BigLinux-Package-Build/build-package/dispatches'' > run-webhooks-aur.sh
+#
+# bash -x run-webhooks-aur.sh
+# rm run-webhooks-aur.sh
+# }
+# ##### NÃO Editar End #####
+
 webhooks() {
-# echo '
-# curl -X POST \
-# -H "Accept: application/json" \
-# -H "Authorization: token '$CHAVE'" \
-# --data '"'{"'"event_type"'": "'"'AUR/$AUR'"'", "'"client_payload"'": { "'"pkgbuild"'": "'""'", "'"branch"'": "'"'stable'"'", "'"url"'": "'"https://aur.archlinux.org/'$AUR'"'", "'"version"'": "'"1.2.3"'"}}'"' \
-# 'https://api.github.com/repos/BigLinux-Package-Build/build-package/dispatches'' > run-webhooks-aur.sh
-# 
-# bash -x run-webhooks-aur.sh
-# rm run-webhooks-aur.sh
-# 
-# sleep 2
-
-#mesa-tkg-git
-echo '
-curl -X POST \
--H "Accept: application/json" \
--H "Authorization: token '$CHAVE'" \
---data '"'{"'"event_type"'": "'"'TKG/$AUR'"'", "'"client_payload"'": { "'"pkgbuild"'": "'""'", "'"branch"'": "'"'stable'"'", "'"url"'": "'"https://github.com/Frogging-Family/mesa-git"'", "'"version"'": "'"1.2.3"'"}}'"' \
-'https://api.github.com/repos/BigLinux-Package-Build/build-package/dispatches'' > run-webhooks-aur.sh
-
-bash -x run-webhooks-aur.sh
-rm run-webhooks-aur.sh
-
-
-# #lib32-mesa-git, esperar o mesa-git buildar e upar pro repo
-# while true :; do
-#     sudo pacman -Sy
-#     verrepo=$(pacman -Ss $pkgname | grep biglinux-stable | grep -v "$pkgname-" | grep -v "\-$pkgname" | grep "$pkgname" | cut -d " " -f2 | cut -d "-" -f1 | sed 's|\.||1' | sed 's|\.||1' | sed 's/_/./' | cut -d "." -f1,5 | sed 's|\.||g')
-#     if [ "$vergit" = "$verrepo" ]; then
-#         break
-#     else
-#         echo "Esperando mesa-git compilar"
-#         sleep 60
-#     fi
-# done
-# 
-# echo '
-# curl -X POST \
-# -H "Accept: application/json" \
-# -H "Authorization: token '$CHAVE'" \
-# --data '"'{"'"event_type"'": "'"'AUR/lib32-mesa-git'"'", "'"client_payload"'": { "'"pkgbuild"'": "'""'", "'"branch"'": "'"'stable'"'", "'"url"'": "'"https://aur.archlinux.org/'lib32-mesa-git'"'", "'"version"'": "'"1.2.3"'"}}'"' \
-# 'https://api.github.com/repos/BigLinux-Package-Build/build-package/dispatches'' > run-webhooks-aur.sh
-# 
-# bash -x run-webhooks-aur.sh
-# rm run-webhooks-aur.sh
-
+curl -X POST -H "Accept: application/json" -H "Authorization: token $CHAVE" --data '{"event_type": "clone", "client_payload": { "branch": "'$branch'", "pkgver": "'$verAurOrg'"}}' https://api.github.com/repos/BigLinuxAur/$package/dispatches
 }
-##### NÃO Editar End #####
 
 #nome do programa como está no pacman
 pkgname=mesa-tkg-git
-#versão online no site da AUR
-# pkgver=$(curl -s https://aur.archlinux.org/cgit/aur.git/tree/PKGBUILD?h=$pkgname | sed 's/<[^>]*>//g' | grep pkgver= | cut -d "=" -f2 | sed 's|\.||g' | sed 's|-||g' | sed 's|_||g')
-# pkgrel=$(curl -s https://aur.archlinux.org/cgit/aur.git/tree/PKGBUILD?h=$pkgname | sed 's/<[^>]*>//g' | grep pkgrel= | cut -d "=" -f2 | sed 's|\.||g' | sed 's|-||g' | sed 's|_||g')
-# versite=$pkgver$pkgrel
 
 #só rodar terça e sexta feira
 if [ "$(date +%u)" = "2" -o "$(date +%u)" = "5" ];then
@@ -95,7 +59,7 @@ if [ "$(date +%u)" = "2" -o "$(date +%u)" = "5" ];then
         echo -e "Enviando \033[01;31m$pkgname\033[0m para Package Build"
         echo "AUR ""$pkgname"="$vergit"
         echo "Repo ""$pkgname"="$verrepo"
-        AUR=$pkgname
+        package=$pkgname
         webhooks
     else
         echo "Versão do $pkgname é igual !"
